c5b4e31d12db49a8a52e55e086707007541b7e55
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-15 13:00:39 +0200

    das

--- PixelTripletHLTGenerator.cc
+++ PixelTripletHLTGenerator.cc
@@ -79,10 +79,10 @@ void PixelTripletHLTGenerator::hitTriplets(const TrackingRegion& region,
   if (theComparitor) theComparitor->init(ev, es);
   
   std::cout<<"INNER LAYER :  " <<pairLayers[0].name()<<std::endl;
-  std::cout<<"Thickness :  " <<pairLayers[0].detLayer()->surface().bounds().thickness();<<std::endl;
+  std::cout<<"Thickness :  " <<pairLayers[0].detLayer()->surface().bounds().thickness()<<std::endl;
   std::cout<<"---------------------------------------"<<std::endl;
   std::cout<<"OUTER LAYER :  " <<pairLayers[1].name()<<std::endl;
-  std::cout<<"Thickness :  " <<pairLayers[1].detLayer()->surface().bounds().thickness();<<std::endl;
+  std::cout<<"Thickness :  " <<pairLayers[1].detLayer()->surface().bounds().thickness()<<std::endl;
   auto const & doublets = thePairGenerator->doublets(region,ev,es, pairLayers);
   std::cout<<"Legacy Doublets : done!"<<std::endl;
   std::cout<<doublets.size()<<" doublets found!"<<std::endl;
