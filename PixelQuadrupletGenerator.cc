5c2649147a71e628be97b92694ed967b32a87cd4
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-18 16:30:39 +0200

    ca

--- PixelQuadrupletGenerator.cc
+++ PixelQuadrupletGenerator.cc
@@ -275,7 +275,7 @@ void PixelQuadrupletGenerator::hitQuadruplets( const TrackingRegion& region, Ord
     std::vector<FKDTree<float,3>*> layersHitsTree;
     
     LayerTree treeFirst; //treeFirst.FKDTree<float,3>::make_FKDTreeFromRegionLayer(fourLayers[0],region,ev,es);
-    layersHitsTree.push_back(&(theKDTreeCache)(fourLayers[0],region,ev,es));
+    layersHitsTree.push_back(&theKDTreeCache(fourLayers[0],region,ev,es));
     LayerTree treeSecond; treeSecond.FKDTree<float,3>::make_FKDTreeFromRegionLayer(fourLayers[1],region,ev,es);
     layersHitsTree.push_back(&treeSecond);
     LayerTree treeThird; treeThird.FKDTree<float,3>::make_FKDTreeFromRegionLayer(fourLayers[2],region,ev,es);
