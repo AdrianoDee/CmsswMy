3ffee3d7be7a618a12f0920dade1a45cb22f5187
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-18 19:18:21 +0200

    key

--- LayerFKDTreeCache.h
+++ LayerFKDTreeCache.h
@@ -46,7 +46,7 @@ public:
   void clear() { theCache.clear(); }
   bool checkCache(int key) {return (!(theCache.get(key)==nullptr)); }
   void getCache(int key,LayerTree* tree){
-        if(checkCache) return theContainer[key];
+        if(checkCache(key)) return theContainer[key];
     }
   void writeCache(int key,LayerTree* tree) {theCache.add(key,tree);}
   
