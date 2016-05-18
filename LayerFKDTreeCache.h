df8434f363fc0ced53fcd7d72cb200606d0f31ac
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-18 16:16:07 +0200

    std::cout<<"HERE IT STARTS ------------------------------------------------------------------------------------"<<std::endl;

--- LayerFKDTreeCache.h
+++ LayerFKDTreeCache.h
@@ -10,7 +10,7 @@
 #include "TrackingTools/TransientTrackingRecHit/interface/SeedingLayerSetsHits.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 
-using Tree = FKDTree<float,3>;
+using LayerTree = FKDTree<float,3>;
 
 class LayerFKDTreeCache {
 //
@@ -22,9 +22,9 @@ private:
     FKDTreeCache(unsigned int initSize) : theContainer(initSize, nullptr){}
     ~FKDTreeCache() { clear(); }
     void resize(int size) { theContainer.resize(size,nullptr); }
-    Tree*  get(int key) { return theContainer[key];}
+    LayerTree*  get(int key) { return theContainer[key];}
     /// add object to cache. It is caller responsibility to check that object is not yet there.
-    void add(int key, Tree * value) {
+    void add(int key, LayerTree * value) {
       if (key>=int(theContainer.size())) resize(key+1);
       theContainer[key]=value;
     }
@@ -33,7 +33,7 @@ private:
       for ( auto & v : theContainer)  { delete v; v=nullptr;}
     }
   private:
-    std::vector< Tree *> theContainer;
+    std::vector<LayerTree *> theContainer;
   private:
     FKDTreeCache(const FKDTreeCache &) { }
   };
@@ -45,23 +45,23 @@ public:
 
   void clear() { theCache.clear(); }
   
-  Tree &
+  LayerTree &
   operator()(const SeedingLayerSetsHits::SeedingLayer& layer, const TrackingRegion & region,
 	     const edm::Event & iE, const edm::EventSetup & iS) {
     int key = layer.index();
     assert (key>=0);
-    Tree * albero = theCache.get(key);
+    LayerTree * cacheTree = theCache.get(key);
     if (albero==nullptr) {
-        albero->FKDTree<float,3>::make_FKDTreeFromRegionLayer(layer,region,iE,iS);
+        cacheTree->FKDTree<float,3>::make_FKDTreeFromRegionLayer(layer,region,iE,iS);
     /*LogDebug("LayerHitMapCache")<<" I got"<< lhm->all().second-lhm->all().first<<" hits in the cache for: "<<layer.detLayer();*/
-      theCache.add(key,albero);
+      theCache.add(key,cacheTree);
     }
     else{
       // std::cout << region.origin() << " " <<  lhm->theOrigin << std::endl;
       LogDebug("LayerFKDTreeCache")<<" FKDTree for layer"<< layer.detLayer()<<" already in the cache with key: "<<key;
     }
     //const Tree result(*buffer);
-    return *albero;
+    return *cacheTree;
   }
 
 private:
