dd375156b666d3136ad311c2cdd865ac90d5ed10
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-19 14:47:14 +0200

    dads

--- LayerFKDTreeCache.h
+++ LayerFKDTreeCache.h
@@ -58,7 +58,7 @@ public:
       const LayerTree * buffer = theCache.get(key);
       if (buffer==nullptr) {
           
-          buffer = new LayerTree::FKDTree(layer,region,iE,iS);
+          buffer = new FKDTree<float,3>::FKDTree(layer,region,iE,iS);
           theCache.add(key,buffer);
           
       }
