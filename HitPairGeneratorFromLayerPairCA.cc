e7bdfd2d2e044d3c155f8a99ea84b79583934b3e
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-19 19:22:01 +0200

    cascas

--- HitPairGeneratorFromLayerPairCA.cc
+++ HitPairGeneratorFromLayerPairCA.cc
@@ -49,7 +49,7 @@ namespace {
     
       void operator()(const LayerTree* tree,const SeedingLayerSetsHits::SeedingLayer& innerLayer,const PixelRecoRange<float>& phiRange,std::vector<unsigned int>& foundHits,Range searchRange) const {
 		  
-		  LayerTree* hereTree = tree;
+		  LayerTree hereTree = &tree;
 		  /*
           constexpr float nSigmaRZ = 3.46410161514f; // std::sqrt(12.f);
           
@@ -125,7 +125,7 @@ namespace {
           const LayerPoint maxPoint(phiRange.max(),zmax,rmax,100000);
           std::cout<<"LayerPoint Max : done!"<<std::endl;
           
-		  hereTree->LayerTree::search_in_the_box(minPoint,maxPoint,foundHits);
+		  hereTree.LayerTree::search_in_the_box(minPoint,maxPoint,foundHits);
           
           std::cout<<"FKDTree Search : done!"<<std::endl;
           
