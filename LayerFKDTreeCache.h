02d81d4efaf234e98646ec179d74e5178f04d741
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-19 14:49:20 +0200

    dasda

--- LayerFKDTreeCache.h
+++ LayerFKDTreeCache.h
@@ -58,7 +58,7 @@ public:
       const LayerTree * buffer = theCache.get(key);
       if (buffer==nullptr) {
           
-          buffer = new FKDTree<float,3>::FKDTree(layer,region,iE,iS);
+          buffer = new FKDTree<TYPE,numberOfDimensions>::FKDTree(layer,region,iE,iS);
           theCache.add(key,buffer);
           
       }
