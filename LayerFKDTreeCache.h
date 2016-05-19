f0a2f1d0e032744ed4f5082ba817c116820f5ec2
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-19 16:48:10 +0200

    cac

--- LayerFKDTreeCache.h
+++ LayerFKDTreeCache.h
@@ -60,7 +60,7 @@ public:
           LayerTree buffer;
           buffer.FKDTree<float,3>::make_FKDTreeFromRegionLayer(layer,region,iE,iS);
           
-          cache = new FKDTree();
+          cache = new <float,3>FKDTree();
           cache = &buffer;
           theCache.add(key,cache);
           
