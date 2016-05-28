9838e91474db375761da5460df8100b11bd84ebc
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-28 19:29:06 +0200

    ->

--- PixelTripletHLTGenerator.cc
+++ PixelTripletHLTGenerator.cc
@@ -90,8 +90,8 @@ void PixelTripletHLTGenerator::hitTriplets(const TrackingRegion& region,
   auto const & doublets = thePairGenerator->doublets(region,ev,es, pairLayers);
   const RecHitsSortedInPhi* innerHitsMap = &(*theLayerCache)(pairLayers[0],region,ev,es);
   const RecHitsSortedInPhi* outerHitsMap = &(*theLayerCache)(pairLayers[1],region,ev,es);
-  std::vector<Hit> innerHits = innerHitsMap.hits();
-  std::vector<Hit> outerHits = innerHitsMap.hits();
+  std::vector<Hit> innerHits = innerHitsMap->hits();
+  std::vector<Hit> outerHits = innerHitsMap->hits();
 
   //std::cout<<"INNER LAYER :  " <<pairLayers[0].name()<<"    "<<"OUTER LAYER :  " <<pairLayers[1].name()<<std::endl;
   //std::cout<<"Legacy Doublets : done!"<<std::endl;
