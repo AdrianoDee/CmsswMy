35a7733b49dcc837fa56e373044594ca55fa5e92
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-06-22 17:37:44 +0200

    detUnitId()

--- SimTracker/SiPixelDigitizer/plugins/SiPixelDigitizer.cc
+++ SimTracker/SiPixelDigitizer/plugins/SiPixelDigitizer.cc
@@ -151,7 +151,7 @@ namespace cms
 
          std::cout<<"================================================"<<std::endl;
          std::cout<<"PSimHit "<<counter<<std::endl;
-         std::cout<<"Track id : "<<(*it).trackId()<<"Particle id : "<<(*it). particleType()<<std::endl;
+         std::cout<<"Track id : "<<(*it).trackId()<<" Particle id : "<<(*it). particleType()<<" Det id: "<<(*it).detUnitId()<<std::endl;
          std::cout<<" x : "<<(*it).localPosition().x()<<" y : "<<(*it).localPosition().y()<<" z : "<<(*it).localPosition().z()<<std::endl;
 
          counter++;
